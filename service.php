d48bb6d8bd6dd91ac0736ffc670c4052cff2e948
kumahacker <kumahavana@gmail.com>
2019-09-01 19:37:02 -0400

    migrate to ejs

--- service.php
+++ service.php
@@ -3,89 +3,84 @@
 use Goutte\Client;
 use GuzzleHttp\Client as GuzzleClient;
 
-class Billboard extends Service
+class BillboardService extends ApretasteService
 {
+
     public $client;
 
-	/**
-	 * Function executed when the service is called
-	 *
-	 * @param Request
-	 * @return Response
-	 * */
-	public function _main(Request $request)
-	{
-		// create a crawler
+    /**
+     * Function executed when the service is called
+     */
+    public function _main()
+    {
+        // create a crawler
         $url = "http://www.billboard.com/charts/hot-100";
 
         $crawler = $this->getCrawler($url);
 
-		// get tracks into array
+        // get tracks into array
         $tracks = [];
 
         $tracks[] = [
-            "rank" => "1",
+            "rank"       => "1",
             "song_title" => $crawler->filter('div.chart-number-one__title')->text(),
-            "artist" => $crawler->filter('div.chart-number-one__artist')->text(),
-            "link" =>  $crawler->filter('div.chart-number-one__lyrics > a')->attr('href')];
+            "artist"     => $crawler->filter('div.chart-number-one__artist')->text(),
+            "link"       => $crawler->filter('div.chart-number-one__lyrics > a')->attr('href')
+        ];
 
-        
 
-		$crawler->filter('div.chart-list-item')->each(function($x) use(&$tracks){
-            $link=
+        $crawler->filter('div.chart-list-item')->each(function ($x) use (&$tracks) {
+            $link =
 
             $tracks[] = [
-                "rank" => $x->attr('data-rank'),
+                "rank"       => $x->attr('data-rank'),
                 "song_title" => $x->attr('data-title'),
-                "artist" => $x->attr('data-artist'),
-                "link" => ($x->filter('div.chart-list-item__lyrics > a')->count()>0)?$x->filter('div.chart-list-item__lyrics > a')->attr('href'):false
+                "artist"     => $x->attr('data-artist'),
+                "link"       => ($x->filter('div.chart-list-item__lyrics > a')->count() > 0) ? $x->filter('div.chart-list-item__lyrics > a')->attr('href') : false
             ];
         });
 
-		// create a json object to send to the template
-		$responseContent = array("tracks" => $tracks);
+        // create a json object to send to the template
+        $responseContent = ["tracks" => $tracks];
 
-		// create the response
-		$response = new Response();
-		$response->setCache("day");
-		$response->setResponseSubject("El Billboard");
-		$response->createFromTemplate("basic.tpl", $responseContent);
-		return $response;
-	}
+        // create the response
+        $this->response->setCache("day");
+        $this->response->setTemplate("basic.ejs", $responseContent);
+    }
 
 
     /**
      * Subservice to return the leter of a song in the billboard list
-     * @param Request
-     * @return Response
      */
+    public function _letra()
+    {
 
-     public function _letra(Request $request){
-         
-         if (strpos($request->query,'www.billboard.com')){
-            $crawler = $this->getCrawler($request->query);
-            if ($crawler->filter('div.lyrics')->count()<1) {
-                $response=new Response();
-                $response->createFromText("Lo sentimos, la letra que usted busca no esta en un formato que podamos procesar");
-                return $response;
+        $query = $this->request->input->data->query;
+        if (strpos($query, 'www.billboard.com')) {
+            $crawler = $this->getCrawler($query);
+            if ($crawler->filter('div.lyrics')->count() < 1) {
+                $this->simpleMessage("No se pudo procesar la letra de la canci&oacute;n",
+                    "Lo sentimos, la letra que usted busca no esta en un formato que podamos procesar");
+
+                return;
             }
+
             $song = $crawler->filter('div.lyrics')->attr('data-lyric-title');
             $artist = $crawler->filter('div.lyrics')->attr('data-lyric-artist');
             $lyrics = $crawler->filter('div.lyrics')->html();
-            
-            $response=new Response();
-            $response->subject="Letra de $song - $artist";
-            $response->setCache("year");
-            $response->createFromTemplate('letra.tpl',array('song'=>$song,'artist'=>$artist,'lyrics'=>$lyrics));
-            return $response;
-         }
-         else{
-            $response=new Response();
-            $response->createFromText("Lo sentimos, la letra que usted busca no se encuentra");
-            return $response;
-         }
-        
-     }
+
+            $this->response->setCache("year");
+            $this->response->setTemplate('letra.ejs', [
+                'song' => $song,
+                'artist' => $artist,
+                'lyrics' => $lyrics
+            ]);
+
+            return;
+        }
+
+        $this->simpleMessage("Letra no encontrada", "Lo sentimos, la letra que usted busca no se encuentra");
+    }
 
     /**
      * Crawler client
@@ -94,12 +89,12 @@ class Billboard extends Service
      */
     public function getClient()
     {
-        if (is_null($this->client))
-        {
+        if (is_null($this->client)) {
             $this->client = new Client();
             $guzzle = new GuzzleClient(["verify" => false]);
             $this->client->setClient($guzzle);
         }
+
         return $this->client;
     }
 
@@ -110,9 +105,12 @@ class Billboard extends Service
      *
      * @return \Symfony\Component\DomCrawler\Crawler
      */
-    protected function getCrawler ($url = "") {
+    protected function getCrawler($url = "")
+    {
         $url = trim($url);
-        if ($url != '' && $url[0] == '/') $url = substr($url, 1);
+        if ($url != '' && $url[0] == '/') {
+            $url = substr($url, 1);
+        }
 
         $crawler = $this->getClient()->request("GET", $url);
 
--- templates/letra.tpl
+++
@@ -1,5 +0,0 @@
-<center>
-    <h1 style="margin: 5px 0px;">Letra de {$song}</h1>
-    <h2>{$artist}</h2>
-    {$lyrics}
-</center>
