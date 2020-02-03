0b4fa3b3e5fa2379661aedaa2c795301d932e6d8
salvipascual <salvi.pascual@gmail.com>
2020-02-03 23:46:05 +0100

    fix billboard

--- service.php
+++ service.php
@@ -7,23 +7,21 @@ use Framework\Crawler;
 
 class Service
 {
-	private $client;
-
 	/**
-	 * Function executed when the service is called
+	 * Displays the 100 songs billboard
 	 *
-	 * @param \Apretaste\Request $request
-	 * @param \Apretaste\Response $response
-	 *
-	 * @throws \Framework\Alert
+	 * @param Request $request
+	 * @param Response $response
 	 */
 	public function _main(Request $request, Response &$response)
 	{
 		// load from cache if exists
-		$cache = TEMP_PATH.date('Ym').'_billboard.tmp';
+		$cache = TEMP_PATH . 'cache/' . date('Ym') . '_billboard.tmp';
 		if (file_exists($cache)) {
 			$content = unserialize(file_get_contents($cache));
-		} // get data from the internet
+		}
+
+		// get data from the internet
 		else {
 			// create a crawler
 			Crawler::start('http://www.billboard.com/charts/hot-100');
@@ -32,8 +30,8 @@ class Service
 			$tracks = [];
 			Crawler::filter('.chart-element__information')->each(function ($x) use (&$tracks) {
 				$tracks[] = [
-						'title' => $x->filter('.chart-element__information__song')->text(),
-						'artist' => $x->filter('.chart-element__information__artist')->text()
+					'title' => $x->filter('.chart-element__information__song')->text(),
+					'artist' => $x->filter('.chart-element__information__artist')->text()
 				];
 			});
 
@@ -44,11 +42,11 @@ class Service
 			file_put_contents($cache, serialize($content));
 		}
 
+		// complete challenge
+		Challenges::complete('view-billboard', $request->person->id);
+
 		// create the response
-		$response->setLayout('billboard.ejs');
 		$response->setCache('month');
 		$response->setTemplate('basic.ejs', $content);
-
-		Challenges::complete('view-billboard', $request->person->id);
 	}
 }
